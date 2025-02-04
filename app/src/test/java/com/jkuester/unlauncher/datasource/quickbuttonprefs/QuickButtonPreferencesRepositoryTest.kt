de3469131f2b8611b5e9ab9bccc9d9869ffac702
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-02-03 21:27:01 -0600

    Clean up mocking in QuickButtonPrefs test

--- app/src/test/java/com/jkuester/unlauncher/datasource/quickbuttonprefs/QuickButtonPreferencesRepositoryTest.kt
+++ app/src/test/java/com/jkuester/unlauncher/datasource/quickbuttonprefs/QuickButtonPreferencesRepositoryTest.kt
@@ -2,96 +2,55 @@ package com.jkuester.unlauncher.datasource.quickbuttonprefs
 
 import androidx.datastore.core.DataStore
 import com.jkuester.unlauncher.datastore.proto.QuickButtonPreferences
-import com.jkuester.unlauncher.datastore.proto.QuickButtonPreferences.QuickButton
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.verify
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
-import kotlin.test.assertSame
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
-private const val ICON_ID = 1
-
 @MockKExtension.CheckUnnecessaryStub
 @MockKExtension.ConfirmVerification
 @ExtendWith(MockKExtension::class)
 class QuickButtonPreferencesRepositoryTest {
-    @Nested
-    inner class IconSettersTest {
-        @MockK
-        lateinit var prefs: QuickButtonPreferences
-        @MockK
-        lateinit var updatedPrefs: QuickButtonPreferences
-        @MockK
-        lateinit var prefBuilder: QuickButtonPreferences.Builder
-        @MockK
-        lateinit var button: QuickButton
-        @MockK
-        lateinit var buttonBuilder: QuickButton.Builder
+    @Test
+    fun setLeftIconId() {
+        val prefs = QuickButtonPreferences.newBuilder().build()
 
-        @BeforeEach
-        fun beforeEach() {
-            every { prefs.toBuilder() } returns prefBuilder
-            every { button.toBuilder() } returns buttonBuilder
-            every { buttonBuilder.setIconId(any()) } returns buttonBuilder
-            every { prefBuilder.build() } returns updatedPrefs
-        }
+        val updatedPrefs = setLeftIconId(QuickButtonIcon.IC_COG.prefId)(prefs)
 
-        @AfterEach
-        fun afterEach() {
-            verify(exactly = 1) { prefs.toBuilder() }
-            verify(exactly = 1) { button.toBuilder() }
-            verify(exactly = 1) { buttonBuilder.setIconId(ICON_ID) }
-            verify(exactly = 1) { prefBuilder.build() }
-        }
+        assertEquals(QuickButtonIcon.IC_COG.prefId, updatedPrefs.leftButton.iconId)
+        assertEquals(0, updatedPrefs.rightButton.iconId)
+        assertEquals(0, updatedPrefs.centerButton.iconId)
+    }
 
-        @Test
-        fun setLeftIconId() {
-            every { prefs.leftButton } returns button
-            every { prefBuilder.setLeftButton(any<QuickButton.Builder>()) } returns prefBuilder
+    @Test
+    fun setCenterIconId() {
+        val prefs = QuickButtonPreferences.newBuilder().build()
 
-            val result = setLeftIconId(ICON_ID)(prefs)
+        val updatedPrefs = setCenterIconId(QuickButtonIcon.IC_COG.prefId)(prefs)
 
-            assertSame(updatedPrefs, result)
-            verify(exactly = 1) { prefs.leftButton }
-            verify(exactly = 1) { prefBuilder.setLeftButton(buttonBuilder) }
-        }
+        assertEquals(QuickButtonIcon.IC_COG.prefId, updatedPrefs.centerButton.iconId)
+        assertEquals(0, updatedPrefs.rightButton.iconId)
+        assertEquals(0, updatedPrefs.leftButton.iconId)
+    }
 
-        @Test
-        fun setCenterIconId() {
-            every { prefs.centerButton } returns button
-            every { prefBuilder.setCenterButton(any<QuickButton.Builder>()) } returns prefBuilder
+    @Test
+    fun setRightIconId() {
+        val prefs = QuickButtonPreferences.newBuilder().build()
 
-            val result = setCenterIconId(ICON_ID)(prefs)
+        val updatedPrefs = setRightIconId(QuickButtonIcon.IC_COG.prefId)(prefs)
 
-            assertSame(updatedPrefs, result)
-            verify(exactly = 1) { prefs.centerButton }
-            verify(exactly = 1) { prefBuilder.setCenterButton(buttonBuilder) }
-        }
-
-        @Test
-        fun setRightIconId() {
-            every { prefs.rightButton } returns button
-            every { prefBuilder.setRightButton(any<QuickButton.Builder>()) } returns prefBuilder
-
-            val result = setRightIconId(ICON_ID)(prefs)
-
-            assertSame(updatedPrefs, result)
-            verify(exactly = 1) { prefs.rightButton }
-            verify(exactly = 1) { prefBuilder.setRightButton(buttonBuilder) }
-        }
+        assertEquals(QuickButtonIcon.IC_COG.prefId, updatedPrefs.rightButton.iconId)
+        assertEquals(0, updatedPrefs.leftButton.iconId)
+        assertEquals(0, updatedPrefs.centerButton.iconId)
     }
 
     @ParameterizedTest
